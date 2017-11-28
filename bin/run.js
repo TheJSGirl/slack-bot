12820d26c5070f55cb845d2d3daba0098a11b921
Priyanka Negi <prinegi100@gmail.com>
2017-11-28 10:49:16 +0530

    init slackClient

--- bin/run.js
+++ bin/run.js
@@ -2,8 +2,15 @@
 
 const service = require('../server/service');
 const http = require('http');
+const slackClient  = require('../server/slackClient');
 
 const server = http.createServer(service);
+
+const slackToken = 'xoxb-277651390864-7eE5Juyy5LMoliaj6WRhndAy';
+const slackLogLevel = 'debug';
+
+const rtm = slackClient.init(slackToken, slackLogLevel);
+rtm.start();
 
 server.listen(3000);
 
