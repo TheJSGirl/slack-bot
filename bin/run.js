bfce9b59dd2ddbcc920e84dfe5c4f2dbc5e7dd63
Priyanka Negi <prinegi100@gmail.com>
2017-11-28 11:35:56 +0530

    connected to slack

--- bin/run.js
+++ bin/run.js
@@ -6,13 +6,16 @@ const slackClient  = require('../server/slackClient');
 
 const server = http.createServer(service);
 
-const slackToken = 'xoxb-277651390864-7eE5Juyy5LMoliaj6WRhndAy';
-const slackLogLevel = 'debug';
+const slackToken = 'xoxb-277651390864-7PSJLS5FWxiZhilz3fkmovC3';
+const slackLogLevel = 'verbose';
 
 const rtm = slackClient.init(slackToken, slackLogLevel);
 rtm.start();
 
-server.listen(3000);
+//subscribe 
+slackClient.addAuthenticatedHandler(rtm, () => {
+    server.listen(3000);
+})
 
 server.on('listening', function () {
   console.log(`microservices is listening on ${server.address().port} in ${service.get('env')}mode.`);
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,10 +1,21 @@
 'use strict';
 
 const RtmClient = require('@slack/client').RtmClient;
-// let token =  'xoxb-277651390864-7eE5Juyy5LMoliaj6WRhndAy';
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+function handleOnAuthenticated(rtmStartData){
+  console.log(`logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function addAuthenticatedHandler(rtm, handler){
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
 
 module.exports.init = function slackClient(token, logLevel){
   
-  const rtm = new RtmClient(token, {logLevel: 'debug'});
+  const rtm = new RtmClient(token, {logLevel: logLevel});
+  addAuthenticatedHandler(rtm, handleOnAuthenticated);
   return rtm;
 }
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
